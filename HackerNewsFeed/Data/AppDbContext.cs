0887a217f84a52da59e047986593dec678671f8b
DeepikaPanday-0126 <deepikapanday0126@gmail.com>
2025-04-20 19:20:37 +0530

    added extra values

--- HackerNewsFeed/Data/AppDbContext.cs
+++ HackerNewsFeed/Data/AppDbContext.cs
@@ -46,7 +46,7 @@ namespace HackerNewsFeed.Data
                     Kids = Array.Empty<int>(),
                     URL = null,
                     Score = 15,
-                    Title = null,
+                    Title = "",
                     Parts = Array.Empty<int>(),
                     Descendants = 90
                 },
@@ -85,6 +85,132 @@ namespace HackerNewsFeed.Data
                     Title = null,
                     Parts = Array.Empty<int>(),
                     Descendants =34
+                },
+                new Items
+                {
+                    Id = 5,
+                    Deleted = false,
+                    Type = "Comment",
+                    By = "abc",
+                    Time = DateTimeOffset.UtcNow.AddMinutes(-20).ToUnixTimeSeconds(),
+                    Text = "Got it",
+                    Dead = false,
+                    Parent = "3",
+                    Poll = 0,
+                    Kids = Array.Empty<int>(),
+                    URL = "http://www.getdropbox.com/u/2/screencast.html",
+                    Score = 5,
+                    Title = null,
+                    Parts = Array.Empty<int>(),
+                    Descendants = 34
+                },
+                new Items
+                {
+                    Id = 6,
+                    Deleted = false,
+                    Type = "comment",
+                    By = "def",
+                    Time = DateTimeOffset.UtcNow.AddMinutes(-20).ToUnixTimeSeconds(),
+                    Text = "Got it",
+                    Dead = false,
+                    Parent = "3",
+                    Poll = 0,
+                    Kids = Array.Empty<int>(),
+                    URL = "http://www.getdropbox.com/u/2/screencast.html",
+                    Score = 5,
+                    Title = null,
+                    Parts = Array.Empty<int>(),
+                    Descendants = 34
+                },
+                new Items
+                {
+                    Id = 7,
+                    Deleted = false,
+                    Type = "comment",
+                    By = "ghi",
+                    Time = DateTimeOffset.UtcNow.AddMinutes(-20).ToUnixTimeSeconds(),
+                    Text = "Got it",
+                    Dead = false,
+                    Parent = "3",
+                    Poll = 0,
+                    Kids = Array.Empty<int>(),
+                    URL = "http://www.getdropbox.com/u/2/screencast.html",
+                    Score = 5,
+                    Title = null,
+                    Parts = Array.Empty<int>(),
+                    Descendants = 34
+                },
+                new Items
+                {
+                    Id = 8,
+                    Deleted = false,
+                    Type = "comment",
+                    By = "jkl",
+                    Time = DateTimeOffset.UtcNow.AddMinutes(-20).ToUnixTimeSeconds(),
+                    Text = "Got it",
+                    Dead = false,
+                    Parent = "3",
+                    Poll = 0,
+                    Kids = Array.Empty<int>(),
+                    URL = "http://www.getdropbox.com/u/2/screencast.html",
+                    Score = 5,
+                    Title = null,
+                    Parts = Array.Empty<int>(),
+                    Descendants = 34
+                },
+                new Items
+                {
+                    Id = 9,
+                    Deleted = false,
+                    Type = "comment",
+                    By = "xyz",
+                    Time = DateTimeOffset.UtcNow.AddMinutes(-20).ToUnixTimeSeconds(),
+                    Text = "Got it",
+                    Dead = false,
+                    Parent = "3",
+                    Poll = 0,
+                    Kids = Array.Empty<int>(),
+                    URL = "http://www.getdropbox.com/u/2/screencast.html",
+                    Score = 5,
+                    Title = null,
+                    Parts = Array.Empty<int>(),
+                    Descendants = 34
+                },
+                new Items
+                {
+                    Id = 10,
+                    Deleted = false,
+                    Type = "comment",
+                    By = "hjk",
+                    Time = DateTimeOffset.UtcNow.AddMinutes(-20).ToUnixTimeSeconds(),
+                    Text = "Got it",
+                    Dead = false,
+                    Parent = "3",
+                    Poll = 0,
+                    Kids = Array.Empty<int>(),
+                    URL = "http://www.getdropbox.com/u/2/screencast.html",
+                    Score = 5,
+                    Title = null,
+                    Parts = Array.Empty<int>(),
+                    Descendants = 34
+                },
+                new Items
+                {
+                    Id = 11,
+                    Deleted = false,
+                    Type = "comment",
+                    By = "iop",
+                    Time = DateTimeOffset.UtcNow.AddMinutes(-20).ToUnixTimeSeconds(),
+                    Text = "Got it",
+                    Dead = false,
+                    Parent = "3",
+                    Poll = 0,
+                    Kids = Array.Empty<int>(),
+                    URL = "http://www.getdropbox.com/u/2/screencast.html",
+                    Score = 5,
+                    Title = null,
+                    Parts = Array.Empty<int>(),
+                    Descendants = 34
                 }
             );
             modelBuilder.Entity<Users>().HasData(
@@ -117,6 +243,86 @@ namespace HackerNewsFeed.Data
                    id = "Jerry",
                    created = DateTimeOffset.UtcNow.AddYears(-1).ToUnixTimeSeconds(),
                    karma = 667,
+                   about = "Technology Analyst.",
+                   submitted = [9128264, 9127792, 9129248,]
+               },
+               new Users
+               {
+                   id = "abc",
+                   created = DateTimeOffset.UtcNow.AddYears(-1).ToUnixTimeSeconds(),
+                   karma = 456,
+                   about = "Technology Analyst.",
+                   submitted = [9128264, 9127792, 9129248,]
+               },
+               new Users
+               {
+                   id = "def",
+                   created = DateTimeOffset.UtcNow.AddYears(-1).ToUnixTimeSeconds(),
+                   karma =789 ,
+                   about = "Technology Analyst.",
+                   submitted = [9128264, 9127792, 9129248,]
+               },
+               new Users
+               {
+                   id = "jkl",
+                   created = DateTimeOffset.UtcNow.AddYears(-1).ToUnixTimeSeconds(),
+                   karma = 234,
+                   about = "Technology Analyst.",
+                   submitted = [9128264, 9127792, 9129248,]
+               },
+               new Users
+               {
+                   id = "iu",
+                   created = DateTimeOffset.UtcNow.AddYears(-1).ToUnixTimeSeconds(),
+                   karma = 789,
+                   about = "Technology Analyst.",
+                   submitted = [9128264, 9127792, 9129248,]
+               },
+               new Users
+               {
+                   id = "bvc",
+                   created = DateTimeOffset.UtcNow.AddYears(-1).ToUnixTimeSeconds(),
+                   karma =89 ,
+                   about = "Technology Analyst.",
+                   submitted = [9128264, 9127792, 9129248,]
+               },
+               new Users
+               {
+                   id = "hytg",
+                   created = DateTimeOffset.UtcNow.AddYears(-1).ToUnixTimeSeconds(),
+                   karma = 56,
+                   about = "Technology Analyst.",
+                   submitted = [9128264, 9127792, 9129248,]
+               },
+               new Users
+               {
+                   id = "uiop",
+                   created = DateTimeOffset.UtcNow.AddYears(-1).ToUnixTimeSeconds(),
+                   karma = 90,
+                   about = "Technology Analyst.",
+                   submitted = [9128264, 9127792, 9129248,]
+               },
+               new Users
+               {
+                   id = "sdf",
+                   created = DateTimeOffset.UtcNow.AddYears(-1).ToUnixTimeSeconds(),
+                   karma = 890,
+                   about = "Technology Analyst.",
+                   submitted = [9128264, 9127792, 9129248,]
+               },
+               new Users
+               {
+                   id = "mnb",
+                   created = DateTimeOffset.UtcNow.AddYears(-1).ToUnixTimeSeconds(),
+                   karma = 123,
+                   about = "Technology Analyst.",
+                   submitted = [9128264, 9127792, 9129248,]
+               },
+               new Users
+               {
+                   id = "xcv",
+                   created = DateTimeOffset.UtcNow.AddYears(-1).ToUnixTimeSeconds(),
+                   karma = 67,
                    about = "Technology Analyst.",
                    submitted = [9128264, 9127792, 9129248,]
                }
